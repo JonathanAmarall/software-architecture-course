5e85fda719f1cc8641a51bcb2223cf49ce5bf7bc
JonathanAmarall <jhouamaral95@gmail.com>
2021-06-28 23:22:46 -0300

    Init Commit

---
+++ tests/NerdStore.Catalogo.Domain.Tests/ProdutoTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace NerdStore.Catalogo.Domain.Tests
+{
+    public class ProdutoTests
+    {
+        [Fact]
+        public void Produto_Validar_ValidacoesDevemRetornarExceptions()
+        {
+            // Arrange & Act & Assert
+
+        }
+    }
+}
